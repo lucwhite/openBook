aa6ab861e94b9ceacde55691511024f9f768484d
Lucas White <lw1944@nyu.edu>
2017-04-09 18:18:56 -0400

    Add files via upload

---
+++ bookScript.js
@@ -0,0 +1,19 @@
+///alert('This is a book');
+
+
+
+function changeImage() {
+
+if (document.getElementById("like").src == "Assets/heartempty.png") 
+	console.log('changingimage');
+    {
+    document.getElementById("like").src = "Assets/heartfull.png";
+        }
+
+    }
+
+
+
+
+
+
